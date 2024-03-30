66f3f8c746af7ad450ff66fa4637f6adc42509c3
t-fi <tobias.fischer@posteo.net>
2024-03-30 18:54:12 +0100

    playing with cubes

--- models/manifold_cubes.py
+++ models/manifold_cubes.py
@@ -2,9 +2,9 @@ from manifold3d import Manifold
 import numpy as np
 from lib.utils import show_manifold
 
-cube_count = 20
+cube_count = 200
 random_points = np.random.uniform(low=-1, high=1, size=(cube_count, 3))
-normalized_points = random_points / np.linalg.norm(random_points, axis=1, keepdims=True) * 1
+normalized_points = random_points / np.linalg.norm(random_points, axis=1, keepdims=True) * 5
 random_axes = np.random.uniform(low=-1, high=1, size=(cube_count, 3))
 random_angles = np.random.uniform(low=0, high=180, size=(cube_count, 3))
 
@@ -15,7 +15,7 @@ for point, angle in zip(normalized_points, random_angles):
     print(point, angle)
     base_box += Manifold.cube([1, 1, 1], True).rotate(angle).translate(point)
 
-base_box ^= sphere
+#base_box ^= sphere
 
 # Main
 
