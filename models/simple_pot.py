44d8ab821bffdc8a78527e6ebe2fcad1ab2f8a08
t-fi <tobias.fischer@posteo.net>
2024-03-31 13:27:12 +0200

    improving the pot

--- models/simple_pot.py
+++ models/simple_pot.py
@@ -1,6 +1,5 @@
 from manifold3d import *
-import numpy as np
-from lib.utils import show_manifold
+from lib.utils import show_manifold, export_manifold
 
 if True:
     set_min_circular_angle(5)
@@ -12,4 +11,7 @@ def bowl(height, radius_low, radius_high, thickness):
     return outer_wall - inner_wall.translate((0, 0, thickness))
 
 
-show_manifold(bowl(20, 90, 100, 5))
+lower_bowl = bowl(20, 90, 100, 2)
+
+export_manifold(lower_bowl, filename="../exported_stl/lower_bowl.stl")
+show_manifold(lower_bowl)
