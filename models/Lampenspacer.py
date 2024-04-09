59ef87f71c2ab2c673cfc7c048ccd0f07aabdd19
t-fi <tobias.fischer@posteo.net>
2024-04-09 22:48:49 +0200

    make it lampenspacy

---
+++ models/Lampenspacer.py
@@ -0,0 +1,14 @@
+from manifold3d import *
+from lib.utils import *
+
+main_body = Manifold.cylinder(176, 25, 25, 100)
+main_body += Manifold.cylinder(170, 19.9, 19.9, 100).translate((0, 0, 10))
+
+main_body += Manifold.cylinder(6, 89, 89, 100, center=True).rotate((90, 0, 0)).translate((0, 0, 89))
+main_body -= Manifold.cylinder(60, 83, 83, 100, center=True).rotate((90, 0, 0)).translate((0, 0, 89))
+
+main_body -= Manifold.cylinder(50, 20.1, 20.1, 100)
+main_body -= Manifold.cylinder(500, 15, 15, 100)
+
+export_manifold(main_body, "../exported_stl/lampenspacer.stl")
+show_manifold(main_body)
